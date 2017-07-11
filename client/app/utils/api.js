9f745089e938c38a45c7f645a562cc29a5233aa3
Yoriyasu Yano <yorinasub17@gmail.com>
2017-07-10 18:24:44 -0700

    WIP hack to force client side to use kong auth system

---
+++ client/app/utils/api.js
@@ -0,0 +1,29 @@
+/* Utilities for accessing the api */
+
+const API_ROOT = '/redash/';
+
+
+function httpPrefixInterceptor($q) {
+  return {
+    request(config) {
+      const url = config.url;
+
+      // ignore template requests
+      if (url.substr(url.length - 5) === '.html') {
+        return config || $q.when(config);
+      }
+
+      config.url = API_ROOT + config.url;
+      return config || $q.when(config);
+    },
+  };
+}
+
+
+export default function configureHttpProvider(ngModule) {
+  // Configure to prepend api root
+  ngModule.factory('httpPrefixInterceptor', httpPrefixInterceptor);
+  ngModule.config(($httpProvider) => {
+    $httpProvider.interceptors.push('httpPrefixInterceptor');
+  });
+}
